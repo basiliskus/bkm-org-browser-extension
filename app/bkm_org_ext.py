40a3b23cee34b3253c31a452a15b03b5103faf64
basiliskus <basilio.bogado@gmail.com>
2020-07-22 21:02:13 -0700

    pep8: remove whitespaces, add blank lines before def, fix indents, remove unused import

--- app/bkm_org_ext.py
+++ app/bkm_org_ext.py
@@ -1,5 +1,4 @@
 import sys
-from datetime import datetime
 from pathlib import Path
 
 from modules import nativemessaging as nm
@@ -17,7 +16,7 @@ logger = log.get_logger('bkm-org', log_path=log_path)
 collection_home = Path(config['bkm-org']['plain_home'])
 default_collection_fpath = Path(config['bkm-org']['collection_fpath'])
 
-ignore_categories = [ 'Bookmarks Menu', 'Bookmarks Toolbar', 'Mobile Bookmarks' ]
+ignore_categories = ['Bookmarks Menu', 'Bookmarks Toolbar', 'Mobile Bookmarks']
 
 current_collection = BookmarkCollection(default_collection_fpath)
 
@@ -30,7 +29,7 @@ while True:
       bookmark = Bookmark(receivedMessage['url'], receivedMessage['title'], created)
       if 'categories' in receivedMessage:
         bookmark.categories = utils.get_category_hierarchy_str(receivedMessage['categories'])
-        bookmark.tags = [ utils.get_tag_from_category(c) for c in receivedMessage['categories'] if not c in ignore_categories ]
+        bookmark.tags = [utils.get_tag_from_category(c) for c in receivedMessage['categories'] if c not in ignore_categories]
       if 'tags' in receivedMessage and receivedMessage['tags']:
         bookmark.add_tags(receivedMessage['tags'].split(','))
       success = current_collection.add(bookmark)
@@ -60,13 +59,13 @@ while True:
       bcc = BookmarkCollectionCatalog('default', collection_home)
       for collection in bcc.collections:
         isdefault = (collection.fpath.name == current_collection.fpath.name)
-        catalog.append({ "filename": collection.fpath.name, "name": collection.name, "description": collection.description, "default": isdefault })
+        catalog.append({"filename": collection.fpath.name, "name": collection.name, "description": collection.description, "default": isdefault})
       nm.send("set-catalog", catalog)
       continue
     if command == 'get-bookmarks':
       bookmarks = []
       for b in current_collection.bookmarks:
-        bookmarks.append({ "url": b.url, "title": b.title, "tags": b.tags })
+        bookmarks.append({"url": b.url, "title": b.title, "tags": b.tags})
       nm.send("set-bookmarks", bookmarks)
       continue
     if command == 'set-current-collection':
